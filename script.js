ed84d9993e7e40071e6985590a05d8a069ec0a16
EmilMinke <em.minke@gmail.com>
2024-10-28 12:53:04 +0100

    feat: draw colors when hovering on squares

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ const gridContainer = document.getElementById("gridContainer");
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.className = "square";
+
+    square.addEventListener("mouseover", function() {
+        this.style.backgroundColor = getRandomColor(); // Change the square color
+    });
+
     gridContainer.appendChild(square);
+}
+
+// helper function to get a random color
+function getRandomColor() {
+    const hexSymbols = "0123456789ABCDEF";
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexSymbols[Math.floor(Math.random() * 16)];
+    }
+    return hexColor;
 }
